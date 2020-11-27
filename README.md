055d8d2916ca3241db8e92ff3b3605e7b931c0a3
Shota Ito <shota.ito.jp@gmail.com>
2020-11-28 01:34:58 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# memo_sinatra
+
+Sinatraでmemoアプリを作るフィヨルドブートキャンプ課題用リポジトリ
+
+# 起動方法
+
+```
+$ bundle install
+$ bundle exec ruby app.rb
+```
---
+++ app.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'json'
+require 'securerandom'
+
+DB_DIRECTORY = './db'
+DB_DIRECTORY.freeze
+
+def memos
+  memos = []
+  Dir.foreach(DB_DIRECTORY) do |f|
+    if File::Stat.new("#{DB_DIRECTORY}/#{f}").ftype == 'file'
+      File.open("#{DB_DIRECTORY}/#{f}") do |j|
+        memos << JSON.parse(File.read(j))
+      end
+    end
+  end
+  memos
+end
+
+get '/' do
+  @memos = memos
+  erb :top
+end
+
+get '/memo' do
+  erb :new_memo
+end
+
+post '/memo' do
+  hash = {}
+  memo_id = SecureRandom.uuid
+  hash['memo_id'] = memo_id
+  hash['title'] = params[:title]
+  hash['body'] = params[:body]
+  File.open("#{DB_DIRECTORY}/#{memo_id}.json", 'a') do |f|
+    f.puts JSON.generate(hash)
+    f.close
+  end
+  redirect to('/')
+end
+
+get '/memo/:memo_id' do
+  @memo_id = params[:memo_id]
+  @file_path = "./#{DB_DIRECTORY}/#{@memo_id}.json"
+  @message = JSON.parse(File.read(@file_path)) if File.exist?(@file_path)
+  erb :show_memo
+end
+
+delete '/memo/:memo_id' do
+  memo_id = params[:memo_id]
+  File.delete("#{DB_DIRECTORY}/#{memo_id}.json")
+  redirect to('/')
+end
+
+patch '/memo/:memo_id' do
+  memo_id = params[:memo_id]
+  title = params[:title]
+  body = params[:body]
+  hash = JSON.parse(File.read("#{DB_DIRECTORY}/#{memo_id}.json"))
+  hash['title'] = title
+  hash['body'] = body
+  File.open("#{DB_DIRECTORY}/#{memo_id}.json", 'w') do |f|
+    f.puts(JSON.generate(hash))
+    f.close
+  end
+  redirect to('/')
+end
+
+get '/editor/:memo_id' do
+  @memo_id = params[:memo_id]
+  @file_path = "./#{DB_DIRECTORY}/#{@memo_id}.json"
+  @message = JSON.parse(File.read(@file_path)) if File.exist?(@file_path)
+  erb :edit_memo
+end
